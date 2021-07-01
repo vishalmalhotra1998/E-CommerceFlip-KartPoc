1c82ec0c7a65e0e138470492403200a3b9b32971
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2021-07-01 11:53:26 +0530

    wip

--- dist/Server.js
+++ dist/Server.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -6,13 +15,25 @@ const routes_1 = require("./routes");
 const Database_1 = require("./libs/Database");
 const notFoundRoute_1 = require("./libs/notFoundRoute");
 const errorHandler_1 = require("./libs/errorHandler");
+// import { createServer } from '';
+const childProcess = require("child_process");
+const redis = require("redis");
+const util_1 = require("util");
 class Server {
     constructor(config) {
         this.config = config;
         this.bootStrap = () => {
             this.initBodyParse();
             this.setUpRootRoutes();
+            this.fileStream();
         };
+        this.fileStream = () => __awaiter(this, void 0, void 0, function* () {
+            // console.log({ __dirname });
+            // // tslint:disable-next-line: no-empty
+            // const data = fs.createReadStream(__dirname + '/someData.txt', { encoding: 'utf8' });
+            // const write = fs.createWriteStream(__dirname + '/data2.text');
+            // data.pipe(write);
+        });
         this.initBodyParse = () => {
             const { app } = this;
             app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,10 +47,52 @@ class Server {
                 });
             });
         };
+        this.longComputation = () => {
+            let sum = 0;
+            for (let i = 0; i < 1e9; i++) {
+                sum = sum + i;
+            }
+            return sum;
+        };
+        this.getAsyncCallForRedis = () => {
+            const { redisServer } = this;
+            return {
+                getAsync: util_1.promisify(redisServer.get).bind(redisServer),
+                setAsync: util_1.promisify(redisServer.set).bind(redisServer)
+            };
+        };
         this.setUpRootRoutes = () => {
-            const { app } = this;
-            app.get('/health-check', (request, response) => {
-                response.send('I am Robot');
+            const { app, redisServer } = this;
+            // app.use(compression());
+            app.get('/health-check', (request, response) => __awaiter(this, void 0, void 0, function* () {
+                const { getAsync, setAsync } = this.getAsyncCallForRedis();
+                const reply = yield getAsync('sums1');
+                if (reply) {
+                    response.send({
+                        sum: JSON.parse(reply)
+                    });
+                }
+                else {
+                    const child = childProcess.fork('./src/fork.ts');
+                    child.send('start');
+                    child.on('message', (sum) => __awaiter(this, void 0, void 0, function* () {
+                        const saveResponse = yield setAsync('sums1', JSON.stringify(sum), 'EX', 5000);
+                        const str = `weeerererr`;
+                        response.send({
+                            sum,
+                            str: str.repeat(100000)
+                        });
+                    }));
+                }
+            }));
+            app.get('/sum', (request, response) => {
+                const sum = this.longComputation();
+                console.time();
+                const sum2 = '----------checkThisWay----------';
+                response.send({
+                    sum,
+                    sum2: sum2.repeat(100000),
+                });
             });
             app.use('/api', routes_1.rootRouting);
             app.use(notFoundRoute_1.notFoundRoute);
@@ -37,6 +100,7 @@ class Server {
         };
         this.app = express();
         this.dataBase = new Database_1.DataBase();
+        this.redisServer = redis.createClient();
     }
 }
 exports.default = Server;
--- src/Server.ts
+++ src/Server.ts
@@ -5,17 +5,33 @@ import { rootRouting } from './routes';
 import { DataBase } from './libs/Database';
 import { notFoundRoute } from './libs/notFoundRoute';
 import { errorHandler } from './libs/errorHandler';
-
+// import { createServer } from '';
+import * as childProcess from 'child_process';
+import * as redis from 'redis';
+import { promisify } from 'util';
+import * as fs from 'fs';
+import * as compression  from 'compression';
 class Server {
   private app: express.Express;
   private dataBase: DataBase;
+  private redisServer: any;
   constructor(private config: any) {
     this.app = express();
     this.dataBase = new DataBase();
+    this.redisServer = redis.createClient();
   }
   bootStrap = (): void => {
     this.initBodyParse();
     this.setUpRootRoutes();
+    this.fileStream();
+  }
+
+  fileStream = async () => {
+    // console.log({ __dirname });
+    // // tslint:disable-next-line: no-empty
+    // const data = fs.createReadStream(__dirname + '/someData.txt', { encoding: 'utf8' });
+    // const write = fs.createWriteStream(__dirname + '/data2.text');
+    // data.pipe(write);
   }
   initBodyParse = () => {
     const { app } = this;
@@ -29,11 +45,56 @@ class Server {
         console.log(':::::::::::Runnig Port::::::::', port);
       });
     });
+
+  }
+
+
+  longComputation = () => {
+    let sum = 0;
+    for (let i = 0; i < 1e9; i++) {
+      sum = sum + i;
+    }
+    return sum;
+  }
+
+  getAsyncCallForRedis = () => {
+    const { redisServer } = this;
+    return {
+      getAsync: promisify(redisServer.get).bind(redisServer),
+      setAsync: promisify(redisServer.set).bind(redisServer)
+    };
   }
   setUpRootRoutes = (): void => {
-    const { app } = this;
-    app.get('/health-check', (request: Request, response: Response) => {
-      response.send('I am Robot');
+    const { app, redisServer } = this;
+    // app.use(compression());
+    app.get('/health-check', async (request: Request, response: Response) => {
+      const { getAsync, setAsync } = this.getAsyncCallForRedis();
+      const reply = await getAsync('sums1');
+      if (reply) {
+        response.send({
+          sum: JSON.parse(reply)
+        });
+      } else {
+        const child = childProcess.fork('./src/fork.ts');
+        child.send('start');
+        child.on('message', async (sum: any) => {
+          const saveResponse = await setAsync('sums1', JSON.stringify(sum), 'EX', 5000);
+          const str = `weeerererr`;
+          response.send({
+            sum,
+            str: str.repeat(100000)
+          });
+        });
+      }
+    });
+    app.get('/sum', (request: Request, response: Response) => {
+      const sum = this.longComputation();
+      console.time();
+      const sum2 = '----------checkThisWay----------'
+      response.send({
+        sum,
+        sum2: sum2.repeat(100000),
+      });
     });
     app.use('/api', rootRouting);
     app.use(notFoundRoute);
